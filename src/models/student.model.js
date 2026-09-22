925558fe2a2ceaab08b38ca2b88e01d69a1f408b
MHM.ANAS <mhm.anas06@gmail.com>
2026-09-22 17:24:36 +0530

    Mongoos Schema & models

---
+++ src/models/student.model.js
@@ -0,0 +1,43 @@
+
+const mongoose = require("mongoose");
+
+const studentSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+
+    phone:{
+        type:String,
+        required:true
+    },
+
+    courese:{
+        type:String,
+        required:true
+    },
+
+    age:{
+        type:Number,
+        required:true
+    },
+
+    status:{
+        type:String,
+        default:"Active"
+    }
+
+    
+},
+    {
+        timestamps: true
+    }
+);
+
+module.exports=mongoose.model("Student", studentSchema);
